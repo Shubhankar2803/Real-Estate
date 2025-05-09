ba6bbd7708d70cb8eda98b93e4676398c62ba268
Shubhankar2803 <singhshubhankar561@gmail.com>
2025-05-09 15:37:05 +0530

    backend done

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,14 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import { authMiddleware } from "./middleware/authMiddleware";
+/* ROUTE IMPORT */
+import tenantRoutes from "./routes/tenantRoutes";
+import managerRoutes from "./routes/managerRoutes";
+import propertyRoutes from "./routes/propertyRoutes";
+import leaseRoutes from "./routes/leaseRoutes";
+import applicationRoutes from "./routes/applicationRoutes";
 
-import tenantRoutes from './routes/tenantRoutes'
-import managerRoutes from './routes/managerRoutes'
-dotenv.config();
- 
+/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -20,15 +23,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-
+/* ROUTES */
 app.get("/", (req, res) => {
-    res.send("This is home route");
-  });
+  res.send("This is home route");
+});
 
-app.use('/tenants',authMiddleware(['tenant']),tenantRoutes)
-app.use('/manager',authMiddleware(["manager"]),managerRoutes)
+app.use("/applications", applicationRoutes);
+app.use("/properties", propertyRoutes);
+app.use("/leases", leaseRoutes);
+app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
+app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
-  const port = Number(process.env.PORT) || 3002;
+/* SERVER */
+const port = Number(process.env.PORT) || 3002;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
