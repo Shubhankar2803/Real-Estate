a0caf8e46091ba1380c4c4f9b1034b7b645cca0a
Shubhankar2803 <singhshubhankar561@gmail.com>
2025-05-10 19:40:21 +0530

    layout fixed nondashboard

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.export={
+    apss:[
+        {
+            name:"project-management",
+            script:"npm",
+            args:"run dev",
+            env:{
+                NODE_ENV:"development"
+            },
+        },
+    ],
+};
